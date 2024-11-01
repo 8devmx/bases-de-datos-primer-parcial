a0018c421d025d5723f6355e4382f979d342ead2
LuzClarita774d <sanchezclarita057@gmail.com>
2024-10-31 19:25:10 -0500

    Formulario
    Agregue un formulario  en "tipos" llamado  "insert.php" en modules  con la función de mostrar datos  de la base de Datos

--- modules/tipos/index.php
+++ modules/tipos/index.php
@@ -11,28 +11,31 @@ require_once '../../lib/db.php';
 
 <body class="d-flex flex-nowrap">
     <?php include_once '../../lib/sidebar.php'; ?>
-    <h1>Tipos</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $consulta = "SELECT * FROM tipos";
-            $resultado = mysqli_query($enlace, $consulta);
-            while ($registro = mysqli_fetch_object($resultado)) {
-            ?>
+    <section class="container p-4 w-100">
+        <h1 class="mb-4">Tipos</h1>
+        
+       
+        <table class="table table-striped table-hover">
+            <thead class="table table-striped table-hover">
                 <tr>
-                    <td><?php echo $registro->id; ?></td>
-                    <td><?php echo $registro->nombre; ?></td>
+                    <th>ID</th>
+                    <th>Nombre</th>
                 </tr>
-            <?php } ?>
-
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php
+                $consulta = "SELECT * FROM tipos";
+                $resultado = mysqli_query($enlace, $consulta);
+                while ($registro = mysqli_fetch_object($resultado)) {
+                ?>
+                    <tr>
+                        <td><?php echo $registro->id; ?></td>
+                        <td><?php echo $registro->nombre; ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </section>
 </body>
 
 </html>
